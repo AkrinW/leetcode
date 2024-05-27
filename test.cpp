354ddc26bd208d23b28e35ac0ccb9497b1d6e711
akarinLaptop <791972041@qq.com>
2024-05-27 14:24:48 +0800

    5.27

--- test.cpp
+++ test.cpp
@@ -3,12 +3,33 @@
 #include <string>
 #include <stack>
 #include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
 #include <queue>
 #include <algorithm>
 #include <cstdint>
 #include <cmath>
 #include <array>
 #include <cctype>
+#include <functional>
+using namespace std;
+
+//优化cin 和cout 速度
+auto __FAST__IO__ = []() noexcept -> int {
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
+    return 0;
+}();
+
+class Solution {
+public:
+
+
+};
+
+int main() {
+    return 0;
+}
 isdigit
 std::array<int, 5> ar;
 double density(double, double);
