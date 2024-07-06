112a6900fb5b95838dc4be1d4d21136e8f0e4592
akarinDesktop <791972041@qq.com>
2024-07-06 23:51:10 +0800

    7.6

---
+++ 2582_PassthePillow.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <algorithm>
+#include <cstdint>
+#include <cmath>
+#include <array>
+#include <cctype>
+#include <functional>
+#include <ranges>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+//优化cin 和cout 速度
+auto __FAST__IO__ = []() noexcept -> int {
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
+    return 0;
+}();
+
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int t = time % (n - 1);
+        return time / (n - 1) % 2 ? n - t : 1 + t;
+    }
+};
+
+
+int main() {
+    return 0;
+}
