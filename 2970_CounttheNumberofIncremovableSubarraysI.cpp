99a9014a20e20d4d0006e63464ac7ebe443ba3dc
akarinDesktop <791972041@qq.com>
2024-07-10 21:31:27 +0800

    7.10

---
+++ 2970_CounttheNumberofIncremovableSubarraysI.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <algorithm>
+#include <cstdint>
+#include <cmath>
+#include <array>
+#include <cctype>
+#include <functional>
+#include <ranges>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+//优化cin 和cout 速度
+auto __FAST__IO__ = []() noexcept -> int {
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
+    return 0;
+}();
+
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        int l = 1;
+        while (l < n && nums[l - 1] < nums[l]) {
+            l++;
+        }
+        res += l + (l < n);
+        for (int r = n - 2; r >= 0; r--) {
+            while (l > 0 && nums[l - 1] >= nums[r + 1]) {
+                l--;
+            }
+            res += l + (l <= r);
+            if (nums[r] >= nums[r + 1]) {
+                break;
+            }
+        }
+        return res;
+    }
+};
+int main() {
+    return 0;
+}
